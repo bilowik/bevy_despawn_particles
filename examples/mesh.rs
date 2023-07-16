c70eca4624712c9bb5e8a7d604f5fe1947c19a08
bilowik <aaronjflores962@gmail.com>
2023-07-16 19:46:47 -0400

    Add mesh example

---
+++ examples/mesh.rs
@@ -0,0 +1,75 @@
+use bevy::prelude::*;
+use bevy_despawn_particles::prelude::*;
+
+#[derive(Component, Default)]
+pub struct Marker;
+
+pub struct MyTimer(pub Timer);
+
+impl Default for MyTimer {
+    fn default() -> Self {
+        Self(Timer::from_seconds(0.5, TimerMode::Once))
+    }
+}
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(DespawnParticlesPlugin)
+        .add_system(setup.on_startup())
+        .add_system(tick)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands, 
+    mut color_materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    commands.spawn(Camera2dBundle::default());
+    commands
+        .spawn(ColorMesh2dBundle {
+            material: color_materials.add(ColorMaterial::from(Color::BLUE)),
+            mesh: meshes.add(Mesh::from(shape::RegularPolygon::new(128.0, 3))).into(),
+            ..default()
+        })
+        .insert(Marker);
+}
+
+fn tick(
+    mut timer: Local<MyTimer>,
+    time: Res<Time>,
+    mut despawn_particles_event_writer: EventWriter<DespawnParticlesEvent>,
+    mut commands: Commands,
+    mut color_materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    marker: Query<Entity, With<Marker>>,
+) {
+    timer.0.tick(time.delta());
+    if timer.0.just_finished() {
+        if let Ok(entity) = marker.get_single() {
+            despawn_particles_event_writer.send(
+                DespawnParticlesEvent::builder()
+                    .with_fade(false)
+                    .with_shrink(false)
+                    .with_linvel(100.0..150.0)
+                    .with_angvel(-0.5..0.5)
+                    .with_lifetime(1.0)
+                    .with_angular_damping(5.0)
+                    .with_linear_damping(8.0)
+                    .build(entity),
+            );
+            timer.0 = Timer::from_seconds(1.2, TimerMode::Once);
+            timer.0.reset();
+        } else {
+            commands
+                .spawn(ColorMesh2dBundle {
+                    material: color_materials.add(ColorMaterial::from(Color::BLUE)),
+            mesh: meshes.add(Mesh::from(shape::RegularPolygon::new(128.0, 3))).into(),
+                    ..default()
+                })
+                .insert(Marker);
+            timer.0 = Timer::from_seconds(0.5, TimerMode::Once);
+        }
+    }
+}
